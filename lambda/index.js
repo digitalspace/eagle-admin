44c89253009b8d7be8dd3c24700a49619515395e
Mark Lisé <mark@digitalspace.ca>
2021-05-13 13:11:55 -0700

    Fix lambda

--- lambda/index.js
+++ lambda/index.js
@@ -1,4 +1,4 @@
-export const handler = async (): Promise<any> => {
+export const handler = async () => {
   const response = {
     statusCode: 200,
     body: JSON.stringify('Hello from lambdaEdge AWS Lambda Function!'),
